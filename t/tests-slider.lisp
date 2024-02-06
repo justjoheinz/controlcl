5d94f05881987c40e97b8ba6d52cbe0bda2d969a
Markus Klink <justjoheinz@gmail.com>
2024-02-06 22:31:55 +0100

    fix test

--- t/tests-slider.lisp
+++ t/tests-slider.lisp
@@ -7,25 +7,23 @@
   (setq *controlcl* (make-instance 'controlcl:controlcl))
   (setq *slider* (make-instance 'slider :id 'slider :name "test-slider"
                                         :x 10 :y 10 :w 100 :h 20
-                                        :value 50 :min-value 0 :max-value 100
-                                        :controlcl *controlcl*)))
-
+                                        :value 50 :min-value 0 :max-value 100)))
 (deftest slider-controller
-  (testing "Slider is correctly initialised"
-    (ok (= (controller-value *slider*) 50)))
+    (testing "Slider is correctly initialised"
+             (ok (= (controller-value *slider*) 50)))
 
   (testing "Setter for value, min-value, max-value. Value cannot be set out of bounds."
-    (ok (= (setf (controller-value *slider*) -1) 0))
-    (ok (= (setf (controller-value *slider*) 101) 100))
-    (ok (= (setf (controller-value *slider*) 50) 50)))
+           (ok (= (setf (controller-value *slider*) -1) 0))
+           (ok (= (setf (controller-value *slider*) 101) 100))
+           (ok (= (setf (controller-value *slider*) 50) 50)))
 
   (testing "Instantiation via controlcl-add-slider"
-    ;; since value is out of bounds
-    (ok (signals  (controlcl-add-slider :id 'slider
-                                        :name "asdf"
-                                        :x 10 :y 10 :w 100 :h 20
-                                        :value -5 :min-value 0 :max-value 100)))
-    ;; id is missing
-    (ok (signals (controlcl-add-slider  :name "asdf"
-                                        :x 10 :y 10 :w 100 :h 20
-                                        :value -5 :min-value 0 :max-value 100)))))
+           ;; since value is out of bounds
+           (ok (signals  (controlcl-add-slider :id 'slider
+                                               :name "asdf"
+                                               :x 10 :y 10 :w 100 :h 20
+                                               :value -5 :min-value 0 :max-value 100)))
+           ;; id is missing
+           (ok (signals (controlcl-add-slider  :name "asdf"
+                                               :x 10 :y 10 :w 100 :h 20
+                                               :value -5 :min-value 0 :max-value 100)))))
